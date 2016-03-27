d4247bf7fd446dbf1db345f679dc9450d629c7a6
Maxim <maxim.v.kochurov@gmail.com>
2016-03-27 23:58:11 +0300

    added docs to TkTools

--- rassadka_modules/tktools.py
+++ rassadka_modules/tktools.py
@@ -3,13 +3,39 @@ from collections import OrderedDict as oDict
 
 
 class TkTools:
-    # Abstract class
+    # Mixin class
 
     @staticmethod
     def _create_menu(parent, dictionary, cascadeopts=None,
                      menuopts=None, itemopts=None, menubindage=None):
         """
         loop initial
+        This method helps to create a tree menu easily
+        the only thing you shulg prepare is a structure 
+        with commands. The structure is dictionary-like.
+        OrderedDict is menu list, dict is command description
+        ##########
+        Usage Example:
+        ##########
+        from collections import OrderedDict as oDict
+        from tktools import TkTools
+        ...
+        class MyClass(TkTools, ...):
+            ...
+            def __init__(...):
+                ...
+                menu = oDict()
+                menu["node1"] = oDict()
+                # create a command
+                menu["node1"]["command1"] = dict(...)
+                # create a cascade as submenu
+                menu["node1"]["subnode1"] = oDict() # subnode will be a cascade menu in main menu
+                # create a command in submenu
+                menu["node1"]["subnode1"]["command2"] = dict(...)
+                # and so on
+                ...
+                self._create_menu(menu)
+        ##########
         :param tk.Menu parent:
         :param oDict | dict dictionary:
         :return:
